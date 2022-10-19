a723dd2a3bcc7a404aaea44b2d19b4373653c842
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2022-10-19 19:55:05 +0530

    hero owners counter and hero image fade animation added

--- dist/js/main.js
+++ dist/js/main.js
@@ -32,3 +32,18 @@ hambtn.addEventListener("click",()=>{
 window.addEventListener('scroll',function(){
     mainHeader.classList.toggle("sticky",window.scrollY > 0);
 })
+
+// Hero 
+let startValue = 1000000
+let endValue = 1278620
+
+let numCount = document.querySelector('.num-count')
+
+let counter = setInterval(function(){
+    startValue += 1565;
+    numCount.innerHTML = `${startValue.toLocaleString()}`
+    if(startValue > endValue){
+        console.log("lol"); 
+        clearInterval(counter)
+    }
+},100)
