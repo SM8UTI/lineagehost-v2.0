6af9fdafb46599985f01c3c4e3b15ceb4cac08fe
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2022-10-31 20:25:03 +0530

    web hosting main js file added

---
+++ dist/js/web-hosting-main.js
@@ -0,0 +1,34 @@
+
+// main header 
+
+const mainHeader = document.getElementById('main-header')
+const navbarItem = document.querySelectorAll('.navbar-items')
+
+navbarItem.forEach((e)=>{ 
+    e.addEventListener("click",()=>{
+        if(e.classList.contains('active')){
+            e.classList.toggle('active')
+        }
+        else{
+            navbarItem.forEach((f)=>{
+                f.classList.remove('active')
+            })
+            e.classList.add('active')
+        }
+    })
+})
+
+const hambtn = document.querySelector('.hamburger')
+
+
+hambtn.addEventListener("click",()=>{
+    mainHeader.classList.toggle('active')
+    document.body.classList.toggle('active');
+  
+})
+
+// scroll or sticky header 
+
+window.addEventListener('scroll',function(){
+    mainHeader.classList.toggle("sticky",window.scrollY > 0);
+})
